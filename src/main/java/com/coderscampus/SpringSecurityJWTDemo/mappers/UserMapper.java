229e1cfc2853b41b9bba42848510b485eb6df024
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-23 20:34:39 -0400

    Update UserMapper.java

--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/mappers/UserMapper.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/mappers/UserMapper.java
@@ -1,7 +1,13 @@
 package com.coderscampus.SpringSecurityJWTDemo.mappers;
 
+import com.coderscampus.SpringSecurityJWTDemo.domain.User;
+import com.coderscampus.SpringSecurityJWTDemo.dto.UserDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", uses = {ProductMapper.class, OrderMapper.class})
 public interface UserMapper {
+
+    UserDto entityToDto (User user);
+
+    User dtoToEntity (UserDto userDto);
 }
