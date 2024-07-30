098de37d324ee39d5d3524d8df4bdf12d1d388d8
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-30 09:09:41 -0400

    Create InventoryController.java

---
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/web/InventoryController.java
@@ -0,0 +1,54 @@
+package com.coderscampus.SpringSecurityJWTDemo.web;
+
+import com.coderscampus.SpringSecurityJWTDemo.service.IngredientService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    private final IngredientService ingredientService;
+//    private final UserServiceImpl userService;
+
+//    @GetMapping("{userId}/home")
+//    public String getUserInventoryHome(@PathVariable Integer userId, ModelMap model) {
+//        User user = userService.findUserById(userId).orElse(null);
+//        model.addAttribute("ingredient", new Ingredient());
+//        model.addAttribute("ingredientsList", ingredientService.findAllIngredientsByUserSorted(user));
+//        model.addAttribute("user", user);
+//        return "ingredient/home";
+//    }
+
+//    @PostMapping("{userId}/create")
+//    public String postCreateIngredient(@ModelAttribute Ingredient ingredient, @PathVariable Integer userId) {
+//        ingredientService.saveIngredientUserRelationship(ingredient, userId);
+//        return "redirect:/inventory/{userId}/home";
+//    }
+//
+//    @GetMapping("/{userId}/{ingredientId}")
+//    public String getIngredientInfoById(@PathVariable Long ingredientId, ModelMap model, @PathVariable Integer userId) {
+//        Ingredient ingredient = ingredientService.findById(ingredientId);
+//        model.addAttribute("user", userService.findUserById(userId).orElse(null));
+//        model.addAttribute("ingredient", ingredient);
+//        model.addAttribute("recipeList", ingredient.getRecipes());
+//        return "ingredient/update";
+//    }
+//
+//    @PostMapping("/{userId}/{ingredientId}")
+//    public String postUpdateIngredientInfo(@PathVariable Integer userId, @ModelAttribute Ingredient ingredient) {
+//        ingredientService.saveIngredientUserRelationship(ingredient, userId);
+//        return "redirect:/inventory/{userId}/home";
+//    }
+//
+//    @PostMapping("/{userId}/{ingredientId}/delete")
+//    public String postDeleteIngredient(@PathVariable Long ingredientId, @PathVariable Integer userId) {
+//        Ingredient ingredient = ingredientService.findById(ingredientId);
+//        User user = userService.findUserById(userId).orElse(null);
+//        ingredientService.delete(ingredient, user);
+//        return "redirect:/inventory/{userId}/home";
+//    }
+}
