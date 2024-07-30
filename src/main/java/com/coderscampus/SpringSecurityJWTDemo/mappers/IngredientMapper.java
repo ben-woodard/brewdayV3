ff90b7abb59c5c44d05cae714c6850822f6baf77
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-30 10:13:35 -0400

    updated mapstruct dependency

--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/mappers/IngredientMapper.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/mappers/IngredientMapper.java
@@ -5,6 +5,7 @@ import com.coderscampus.SpringSecurityJWTDemo.dto.IngredientDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", uses = {RecipeMapper.class, OrderMapper.class})
+
 public interface IngredientMapper {
 
     IngredientDto entityToDto(Ingredient ingredient);
