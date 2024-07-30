bb5f5c825324bf5df487e90d91ee67fe35b01e7b
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-07-30 09:22:14 -0400

    updated ingredient/ user relationship and seeder data

--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/Seeder.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/Seeder.java
@@ -1,7 +1,9 @@
 package com.coderscampus.SpringSecurityJWTDemo;
 
 import com.coderscampus.SpringSecurityJWTDemo.domain.Ingredient;
+import com.coderscampus.SpringSecurityJWTDemo.domain.User;
 import com.coderscampus.SpringSecurityJWTDemo.repository.IngredientRepository;
+import com.coderscampus.SpringSecurityJWTDemo.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,9 +14,19 @@ import java.util.Arrays;
 @AllArgsConstructor
 public class Seeder implements CommandLineRunner {
     private final IngredientRepository ingredientRepo;
+    private final UserRepository userRepo;
 
     @Override
     public void run(String... args) throws Exception {
+
+        User user1 = new User();
+        user1.setFirstName("Joe");
+        user1.setLastName("Schmo");
+        user1.setCompanyName("Joe's Brewing");
+        user1.setEmail("joe@joe.com");
+        user1.setPassword("vvvv");
+
+        userRepo.saveAndFlush(user1);
 
         Ingredient ingredient1 = new Ingredient();
         ingredient1.setIngredientName("2-Row Barley");
@@ -22,6 +34,7 @@ public class Seeder implements CommandLineRunner {
         ingredient1.setOrderingThreshold(50.0);
         ingredient1.setIngredientType(Ingredient.ingredientType.MALT);
         ingredient1.setUnitOfMeasurement(Ingredient.unitOfMeasurement.lBS);
+        ingredient1.setUser(user1);
 
         Ingredient ingredient2 = new Ingredient();
         ingredient2.setIngredientName("Citra");
@@ -29,6 +42,7 @@ public class Seeder implements CommandLineRunner {
         ingredient2.setOrderingThreshold(10.0);
         ingredient2.setIngredientType(Ingredient.ingredientType.HOP);
         ingredient2.setUnitOfMeasurement(Ingredient.unitOfMeasurement.lBS);
+        ingredient2.setUser(user1);
 
         Ingredient ingredient3 = new Ingredient();
         ingredient3.setIngredientName("Citra");
@@ -36,9 +50,12 @@ public class Seeder implements CommandLineRunner {
         ingredient3.setOrderingThreshold(10.0);
         ingredient3.setIngredientType(Ingredient.ingredientType.HOP);
         ingredient3.setUnitOfMeasurement(Ingredient.unitOfMeasurement.lBS);
+        ingredient3.setUser(user1);
+
+        user1.getIngredients().addAll(Arrays.asList(
+                new Ingredient[]{ingredient1, ingredient2, ingredient3}));
 
         ingredientRepo.saveAllAndFlush(Arrays.asList(
                 new Ingredient[]{ingredient1, ingredient2, ingredient3}));
-
     }
 }
