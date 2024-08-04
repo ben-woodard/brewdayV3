402d33d34cd6a066d1779051135420698cf3c11f
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-08-04 10:26:16 -0400

    created AdminController
    moved admin related code from user Controller to admin Controler

--- src/main/java/com/coderscampus/SpringSecurityJWTDemo/web/UserController.java
+++ src/main/java/com/coderscampus/SpringSecurityJWTDemo/web/UserController.java
@@ -1,59 +1,20 @@
 package com.coderscampus.SpringSecurityJWTDemo.web;
 
-import com.coderscampus.SpringSecurityJWTDemo.domain.Authority;
-import com.coderscampus.SpringSecurityJWTDemo.domain.User;
-import jakarta.annotation.PostConstruct;
+
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.coderscampus.SpringSecurityJWTDemo.dto.request.SignUpRequest;
 import com.coderscampus.SpringSecurityJWTDemo.dto.response.JwtAuthenticationResponse;
 import com.coderscampus.SpringSecurityJWTDemo.security.AuthenticationServiceImpl;
-import com.coderscampus.SpringSecurityJWTDemo.security.JwtServiceImpl;
-import com.coderscampus.SpringSecurityJWTDemo.service.RefreshTokenService;
-import com.coderscampus.SpringSecurityJWTDemo.service.impl.UserServiceImpl;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 @RestController
+@AllArgsConstructor
 public class UserController {
 
-    private UserServiceImpl userService;
     private AuthenticationServiceImpl authenticationService;
-    private JwtServiceImpl jwtService;
-    private RefreshTokenService refreshTokenService;
-    private PasswordEncoder passwordEncoder;
-
-    public UserController(UserServiceImpl userService, AuthenticationServiceImpl authenticationService,
-                          JwtServiceImpl jwtService, RefreshTokenService refreshTokenService, PasswordEncoder passwordEncoder) {
-        super();
-        this.userService = userService;
-        this.authenticationService = authenticationService;
-        this.jwtService = jwtService;
-        this.refreshTokenService = refreshTokenService;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostConstruct
-    public void init() {
-        createAdminUser();
-    }
-
-    public void createAdminUser() {
-        User adminUser = new User();
-        adminUser.setFirstName("Admin");
-        adminUser.setLastName("User");
-        adminUser.setEmail("admin@email.com");
-        adminUser.setPassword(passwordEncoder.encode("adminPassword"));
-        Authority adminAuth = new Authority("ROLE_ADMIN", adminUser);
-        adminUser.setAuthorities(Collections.singletonList(adminAuth));
-        userService.save(adminUser);
-
-    }
 
     @PostMapping("/register")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
